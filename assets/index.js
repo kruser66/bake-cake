0bccf6ac26339bbc076b3efea00f3d70ca263f45
kruser <kruser@yandex.ru>
2023-07-18 19:45:18 +0500

    initial commit

---
+++ assets/index.js
@@ -0,0 +1,138 @@
+console.log(db_data);
+console.log(db_costs);
+
+Vue.createApp({
+    name: "App",
+    components: {
+        VForm: VeeValidate.Form,
+        VField: VeeValidate.Field,
+        ErrorMessage: VeeValidate.ErrorMessage,
+    },
+    data() {
+        return {
+            schema1: {
+                lvls: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' количество уровней';
+                },
+                form: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' форму торта';
+                },
+                topping: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' топпинг';
+                }
+            },
+            schema2: {
+                name: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' имя';
+                },
+                phone: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' телефон';
+                },
+                name_format: (value) => {
+                    const regex = /^[a-zA-Zа-яА-Я]+$/
+                    if (!value) {
+                        return true;
+                    }
+                    if ( !regex.test(value)) {
+
+                        return '⚠ Формат имени нарушен';
+                    }
+                    return true;
+                },
+                email_format: (value) => {
+                    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+                    if (!value) {
+                        return true;
+                    }
+                    if ( !regex.test(value)) {
+
+                        return '⚠ Формат почты нарушен';
+                    }
+                    return true;
+                },
+                phone_format:(value) => {
+                    const regex = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+                    if (!value) {
+                        return true;
+                    }
+                    if ( !regex.test(value)) {
+
+                        return '⚠ Формат телефона нарушен';
+                    }
+                    return true;
+                },
+                email: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' почту';
+                },
+                address: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' адрес';
+                },
+                date: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' дату доставки';
+                },
+                time: (value) => {
+                    if (value) {
+                        return true;
+                    }
+                    return ' время доставки';
+                }
+            },
+            DATA: db_data,
+            Costs: db_costs,
+            Levels: 0,
+            Form: 0,
+            Topping: 0,
+            Berries: 0,
+            Decor: 0,
+            Words: '',
+            Comments: '',
+            Designed: false,
+
+            Name: '',
+            Phone: null,
+            Email: null,
+            Address: null,
+            Dates: null,
+            Time: null,
+            DelivComments: ''
+        }
+    },
+    methods: {
+        ToStep4() {
+            this.Designed = true
+            setTimeout(() => this.$refs.ToStep4.click(), 0);
+        }
+    },
+    computed: {
+        Cost() {
+            let W = this.Words ? this.Costs.Words : 0
+            return this.Costs.Levels[this.Levels] + this.Costs.Forms[this.Form] +
+                this.Costs.Toppings[this.Topping] + this.Costs.Berries[this.Berries] +
+                this.Costs.Decors[this.Decor] + W
+        }
+    }
+}).mount('#VueApp')
---
+++ main/views.py
@@ -0,0 +1,36 @@
+import json
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+db_costs = {
+    'Levels': [0, 400, 750, 1100],
+    'Forms': [0, 600, 400, 1000],
+    'Toppings': [0, 0, 200, 180, 200, 300, 350, 200],
+    'Berries': [0, 400, 300, 450, 500],
+    'Decors': [0, 300, 400, 350, 300, 200, 280],
+    'Words': 500
+}
+
+
+db_data = {
+    'Levels': ['не выбрано', '1', '2', '3'],
+    'Forms': ['не выбрано', 'Круг', 'Квадрат', 'Ромб'],
+    'Toppings': ['не выбрано', 'Без', 'Белый соус', 'Карамельный', 'Кленовый', 'Черничный', 'Молочный шоколад', 'Клубничный'],
+    'Berries': ['нет', 'Ежевика', 'Малина', 'Голубика', 'Клубника'],
+    'Decors': [ 'нет', 'Фисташки', 'Безе', 'Фундук', 'Пекан', 'Маршмеллоу', 'Марципан']
+}
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["db_costs"] = json.dumps(db_costs)
+        context["db_data"] = json.dumps(db_data)
+        
+        print(context)
+        return context
+    
