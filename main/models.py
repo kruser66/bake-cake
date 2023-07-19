d8ac33cb95e2f20bd7576a9d95347b833044ecbf
kruser <kruser@yandex.ru>
2023-07-19 19:43:08 +0500

    update models

--- main/models.py
+++ main/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
-    fullname = models.CharField(verbose_name='Имя', max_length=255)
+class CakeUser(User):
+    name = models.CharField(verbose_name='Имя', max_length=255)
     phone = PhoneNumberField(
         'Номер клиента',
         blank=True,
@@ -13,102 +14,48 @@ class User(models.Model):
     address = models.CharField(verbose_name='Адрес', max_length=255)
 
     class Meta:
-        verbose_name = ('Клиент')
-        verbose_name_plural = ('Клиенты')
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
 
     def __str__(self):
-        return self.fullname
+        return self.phone
+
+
+class CategoryCake(models.Model):
+
+    name = models.CharField('Наименование категории', max_length=100)
+
+    class Meta:
+        verbose_name = 'категория торта'
+        verbose_name_plural = 'категории тортов'
+
+    def __str__(self):
+        return self.name
 
 
 class Cake(models.Model):
-    CATEGORIES = [
-        ('Birthday', 'День рождение'),
-        ('Teaparty', 'Чаепитие'),
-        ('Wedding', 'Свадьба')
-    ]
-    LEVELS = [
-        ('One level', '1 уровень'),
-        ('Two level', '2 уровня'),
-        ('Three level', '3 уровня')
-    ]
-    FORMS = [
-        ('Square', 'Квадрат'),
-        ('Circle', 'Круг'),
-        ('Rectangle', 'Прямоугольник')
-    ]
-    TOPPINGS = [
-        ('Without', 'Без топпинга'),
-        ('White Sauce', 'Белый соус'),
-        ('Caramel syrup', 'Карамельный сироп'),
-        ('Maple syrup', 'Кленовый сироп'),
-        ('Blueberry syrup', 'Черничный сироп'),
-        ('Milk chocolate', 'Молочный шоколад'),
-    ]
-    BERRIES = [
-        ('Blackberry', 'Ежевика'),
-        ('Raspberry', 'Малина'),
-        ('Blueberry', 'Голубика'),
-        ('Strawberry', 'Клубника'),
-    ]
-    DECORS = [
-        ('Pistachios', 'Фисташки'),
-        ('Meringue', 'Безе'),
-        ('Hazelnuts', 'Фундук'),
-        ('Pecan', 'Пекан'),
-        ('Marshmallow', 'Маршмеллоу'),
-        ('Marzipan', 'Марципан'),
-    ]
+    
     title = models.CharField(verbose_name='Название торта', max_length=255)
-    description = models.CharField(
+    description = models.TextField(
         verbose_name='Описание торта',
-        max_length=255
+        max_length=500
     )
     standard = models.BooleanField(verbose_name='Торт стандартный?')
-    category = models.CharField(
-        verbose_name='Категория',
-        max_length=10,
-        choices=CATEGORIES,
-    )
+    category = models.ForeignKey(CategoryCake, on_delete=models.CASCADE, null=True, blank=True)
     price = models.PositiveIntegerField(verbose_name='Цена')
-    level = models.CharField(
-        verbose_name='Кол-во уровней',
-        max_length=10,
-        choices=LEVELS,
-    )
-    form = models.CharField(
-        verbose_name='Форма торта',
-        max_length=10,
-        choices=FORMS,
-    )
-    topping = models.CharField(
-        verbose_name='Топпинг',
-        max_length=10,
-        choices=TOPPINGS,
-    )
-    berry = models.CharField(
-        verbose_name='Ягоды',
-        max_length=10,
-        choices=BERRIES,
-        blank=True,
-        default='',
-    )
-    decor = models.CharField(
-        verbose_name='Декор',
-        max_length=10,
-        choices=DECORS,
-        blank=True,
-        default='',
-    )
-    caption = models.CharField(
-        verbose_name='Мы можем разместить на торте любую надпись,\
-            например: «С днем рождения!»', max_length=255)
 
 
 class Order(models.Model):
     client = models.ForeignKey(
-        User,
+        CakeUser,
         on_delete=models.CASCADE,
-        verbose_name='Клиент'
+        verbose_name='Клиент',
+        related_name='orders'
+    )
+    cake = models.ForeignKey(
+        Cake,
+        on_delete=models.CASCADE,
+        verbose_name='Торт'
     )
     date_delivery = models.DateField(
         verbose_name='Дата доставки',
@@ -124,13 +71,21 @@ class Order(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    status = models.CharField(
+        'Статус заказа',
+        max_length=20,
+        choices=[
+            ('done', 'Выполнен'),
+            ('delivery', 'В доставке')
+        ]
+    )
+    
     class Meta:
-        verbose_name = ('Заказ')
-        verbose_name_plural = ('Заказы')
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
 
     def __str__(self):
-        return str(self.id)
+        return f'{self.id - {self.client} - {self.ca}}'
 
 
 class OptionType(models.Model):
