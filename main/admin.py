b9b842962840b6e232529262ed06c7d3ddeabdcb
kruser <kruser@yandex.ru>
2023-07-20 15:51:54 +0500

    undo last change

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
+from django.utils.html import format_html
+
 from main.models import OptionType, OptionPrice, CakeUser, CategoryCake, Cake, Order
 
+
+def format_preview_image(image, height='200px'):
+    return format_html(
+        '<img src="{url}" height="{height}"/>',
+        url=image.image.url,
+        height=height,
+    )
 
 class OptionPriceInline(admin.TabularInline):
     model = OptionPrice
@@ -26,10 +35,10 @@ class CategoryCakeAdmin(admin.ModelAdmin):
 
 @admin.register(Cake)
 class CakeAdmin(admin.ModelAdmin):
-    readonly_fields = ["preview"]
+    readonly_fields = [format_preview_image]
 
-    def preview(self, obj):
-        return mark_safe(f'<img src="{obj.img.url}" style="max-height: 200px;">')
+    # def preview(self, obj):
+    #     return mark_safe(f'<img src="{obj.img.url}" style="max-height: 200px;">')
 
 
 @admin.register(Order)
